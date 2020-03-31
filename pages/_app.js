18891f76c21e0253772684cc70c1271e2146a62e
Nick Chapman <nchapman@gmail.com>
2020-03-30 18:59:11 -0700

    Initial matomo integration.

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,23 @@
+import App from 'next/app';
+import { initMatomo } from '../lib/matomo';
 import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
 config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
 
-
 import '../styles/app.scss'
 
+export default class MyApp extends App {
+  componentDidMount() {
+    initMatomo({
+      siteId: 1,
+      matomoUrl: 'https://matomo.bipbop.me',
+      cookieDomain: '*.bipbop.me'
+    });
+  }
 
-// This default export is required in a new `pages/_app.js` file.
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  render() {
+    const { Component, pageProps } = this.props;
+
+    return <Component {...pageProps} />
+  }
 }

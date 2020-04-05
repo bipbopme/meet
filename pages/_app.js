bd12b1ab3352b1584ef803610829946d4c841e2c
Nick Chapman <nchapman@gmail.com>
2020-04-05 15:00:51 -0700

    Switched to dotenv for development.

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import { initMatomo } from '../lib/matomo'
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import '../styles/app.scss'
+
 config.autoAddCss = false // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
 
 export default class MyApp extends App {

52404876bdcad51f72d53227a9b35959175f4a6e
Nick Chapman <nchapman@gmail.com>
2020-04-22 14:03:06 -0700

    Automated lib-jitsi-meet build and inclusion.

--- next.config.js
+++ next.config.js
@@ -4,5 +4,15 @@ module.exports = {
   env: {
     MATOMO_CONFIG: process.env.MATOMO_CONFIG,
     JITSI_CONFIG: process.env.JITSI_CONFIG,
+  },
+  webpack: (config, { isServer, webpack }) => {
+    if (!isServer) {
+      config.plugins.push(new webpack.ProvidePlugin({
+        $: 'jquery',
+        JitsiMeetJS: __dirname + '/dist/lib-jitsi-meet.min.js'
+      }))
+    }
+
+    return config
   }
 }

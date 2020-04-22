44c41801cdec096f3c933b2498c6f51314abaa12
Nick Chapman <nchapman@gmail.com>
2020-04-21 18:46:55 -0700

    Disabled video cropping for shared screens.

--- components/videoChat/controls/videoChatControls.js
+++ components/videoChat/controls/videoChatControls.js
@@ -4,12 +4,20 @@ import ScreenShareButton from './screenShareButton'
 import VideoButton from './videoButton'
 
 export default class VideoChatControls extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.canScreenCapture = !!navigator.mediaDevices.getDisplayMedia
+  }
+
   render () {
     return (
       <footer className='controls'>
         <MicButton localParticipant={this.props.localParticipant} />
         <VideoButton localParticipant={this.props.localParticipant} />
-        <ScreenShareButton localParticipant={this.props.localParticipant} />
+        {this.canScreenCapture &&
+          <ScreenShareButton localParticipant={this.props.localParticipant} />
+        }
       </footer>
     )
   }

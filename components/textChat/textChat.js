64bab2e504d5e8cde5c5f35bad6c4e09dc5c1ce1
Nick Chapman <nchapman@gmail.com>
2020-04-22 18:17:12 -0700

    Replaced the scroll to bottom component with a simpler implementation.

--- components/textChat/textChat.js
+++ components/textChat/textChat.js
@@ -1,22 +1,37 @@
 import ChatBox from './chatBox'
 import Message from './message'
 import React from 'react'
-import ScrollToBottom from 'react-scroll-to-bottom'
 import { observer } from 'mobx-react'
 
 @observer
 export default class TextChat extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.messagesRef = React.createRef()
+  }
+
+  getSnapshotBeforeUpdate (prevProps, prevState) {
+    return this.messagesRef.current.scrollHeight - this.messagesRef.current.scrollTop === this.messagesRef.current.clientHeight
+  }
+
+  componentDidUpdate (prevProps, prevState, wasScrolledToBottom) {
+    if (wasScrolledToBottom) {
+      this.messagesRef.current.scrollTop = this.messagesRef.current.scrollHeight
+    }
+  }
+
   render () {
     const { messages } = this.props.conference
 
     return (
       <div className='textChat'>
         <header />
-        <ScrollToBottom className='messages' checkInterval={50}>
+        <div className='messages' ref={this.messagesRef}>
           {messages.map(message => (
             <Message key={message.id} message={message} />
           ))}
-        </ScrollToBottom>
+        </div>
         <ChatBox conference={this.props.conference} />
       </div>
     )

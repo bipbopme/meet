d828068f9390d2e550b0629ad8ebe4068c1d164e
Nick Chapman <nchapman@gmail.com>
2020-06-05 12:48:57 -0700

    Removed fontawesome.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,7 @@
 import "../assets/styles/app.scss";
-import { config } from "@fortawesome/fontawesome-svg-core";
 import { initMatomo } from "../lib/matomo";
 import App from "next/app";
 import React from "react";
-
-config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
 
 export default class MyApp extends App {
   componentDidMount(): void {

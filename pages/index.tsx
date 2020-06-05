ea7a1218215c76a7a2c4791a76ca575c3fef3ccb
Nick Chapman <nchapman@gmail.com>
2020-06-05 16:24:59 -0700

    First pass at a new home page.

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,13 +13,28 @@ export default class HomePage extends React.Component {
     return (
       <div className="homePage">
         <Head>
-          <title>bipbop.</title>
+          <title>bipbop | free video chat in your browser</title>
         </Head>
-        <h1>bipbop</h1>
-        <h2>easy video chats in your browser</h2>
-        <Button type="primary" size="large" shape="round" onClick={this.handleClick}>
-          Start
-        </Button>
+        <header>
+          <h1>bipbop</h1>
+        </header>
+        <section>
+          <div className="illustration">
+            <img src="/images/people@2x.png" />
+          </div>
+          <div className="pitch">
+            <h2>Video chat with your favorite people for free.</h2>
+            <div className="copy">
+              There&apos;s nothing to install. Just send your friends a link and start chatting.
+              From any device. Anywhere.
+            </div>
+            <div className="buttons">
+              <Button type="primary" size="large" shape="round" onClick={this.handleClick}>
+                Start a chat
+              </Button>
+            </div>
+          </div>
+        </section>
       </div>
     );
   }
